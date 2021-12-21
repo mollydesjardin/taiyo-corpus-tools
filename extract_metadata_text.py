16046237b0cfc816f1e5406e9d0b3ebaff214a59
Molly Des Jardin <mdesjardin@gmail.com>
2021-12-21 09:15:54 -0500

    Tested and debugged existing code to read in preprocessed UTF-8 XML files with ASCII tag names and attributes; parse files (one magazine issue per file) by article and retrieve metadata per-article from XML tag attributes. Store metadata in a list of lists (previously this was a dictionary which made less sense as a data structure here) and write out article text into individual .txt files as it goes. The metadata is written out as a CSV at the end of the script after all article text files are output.

--- extract_metadata_text.py
+++ extract_metadata_text.py
@@ -1,37 +1,56 @@
 # -*- coding: utf-8 -*-
 
 """
-global metadata -> year, month or issue number
-kiji -> extract metadata (title, author, column, style, genre)
+This script reads in each XML file containing an issue's worth of articles from
+the Taiyo magazine CD-ROM published by NINJAL, in the XML directory on the CD.
+The input filenames contain year/month (ex. t189501.xml = January 1895). Other
+metadata per-article is retrieved from attributes in the "kiji" tag which
+indicates start/end of each article.
+Output is a plain text file, with no XML tags and no tokenization (whitespace)
+inserted between words, with filename convention:
+    year/date_author_title
+And a CSV file containing all metadata per-article:
 
-remove <s></s> (not between), odoriji, <br />, <l />, ketsuji and things between, gaiji (not between), inyo
+per file: year, month of issue (from filename)
+per article tag within file: title, author, column, style, genre
+
+Other cleanup...
+remove tags/attrs but retain text enclosed between:
+s, gaiji (外字), chu (注), ligature (合字), quote (引用), ketsuji (敬意欠字), odoriji (踊字)
+remove tags that don't enclose any text: br, l
+(because no tags need to have text between them removed, this is all achieved
+ with article.text)
+
 """
 
 
 from bs4 import BeautifulSoup as BS
 import glob, csv
 
-metadata_keys = ['title', 'author', 'column', 'style', 'genre']
-
-with open('taiyo.csv', 'w', encoding='utf-8') as csvfile:
-	writer = csv.DictWriter(csvfile, fieldnames=metadata_keys)
-	writer.writeheader()
+metadata_keys = ['author','title','column','style','genre']
+articles_list = []
     
-	# For each file in directory, retrieve file
-	for filename in glob.iglob("*.xml"):
-		article_prefix = filename[3:-4]
+for infile in glob.iglob("*.xml"):
+# Get metadata and put in dict. Write out file with text only as cleaned
+    with open(infile, 'r', encoding='utf-8') as soup_in:
+        soup = BS(soup_in, 'lxml')
+        issuedate = infile[3:-4]
 
-	# Get metadata and write to CSV
-		with open(filename, 'r', encoding='utf-8') as soup_in:
-			soup = BS(soup_in, "lxml")
+# saving metadata for writing out later
+        for article in soup('article'):
+            article_md = []
+            article_md.append(issuedate)
+            for key in metadata_keys:
+                article_md.append(article.attrs[key])
+            articles_list.append(article_md)
 
-			for article in soup('kiji'):
-				article_data = {key: article.attrs[key] for key in metadata_keys}
-				writer.writerow(article_data)
-			
-		#save article text
-				article_filename = '{}_{}_{}.txt'.format(article_prefix, article_data['author'], article_data['title'])
-				if article.ketsuji:
-					article.ketsuji.decompose()
-				with open(article_filename, 'w', encoding='utf-8') as output_file:
-					output_file.write(article.get_text())
+            article_filename = 'articles/{}_{}_{}.txt'.format(article_md[0], article_md[1], article_md[2])
+
+            with open(article_filename, 'w', encoding='utf-8') as output_file:
+                output_file.write(article.text.strip())
+
+# write header from metadata_keys then write each row of md in list
+with open('taiyo_metadata.csv', 'w', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['issue'] + metadata_keys)
+    writer.writerows(articles_list)
